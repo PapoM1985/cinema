5f60971de316ae4ceef29aeb831b24d9675c43c7
papo <paolo.marra1985@gmail.com>
2022-10-20 09:25:51 +0200

    start project cinema

---
+++ src/main/java/com/cinema/cinema/ManagementCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementCinemaApplication.class, args);
+	}
+
+}
